8062805316f24f629efb85e292cf1852fd3de6a9
pcmagas <pcmagas@disroot.org>
2022-12-21 00:06:57 +0200

    Keep track of manipuilator progress.

--- src/manipulators/redirect.js
+++ src/manipulators/redirect.js
@@ -9,10 +9,12 @@ module.exports.redirectToHttps = (db,http_only,req,res)=>{
     const url_host_only = req.get('host');
    
     const sql = "SELECT * from redirect where use_in_http = 1 and exact_match = 1 and url_from = ? and exact_match=1 LIMIT 1";
-    db.serialize(function() {
-        const stmt = db.prepare(sql);
-        const value = stmt.run(fullUrl)
-        stmt.finalize();
-        
+    const sqlNonExact = "SELECT * from redirect where use_in_http = 1 and exact_match = 1 and url_from = ? and exact_match=1 LIMIT 1"
+
+    db.all(sql,fullUrl,function(err, rows) {
+        if(err){
+            console.error(err)
+        }
+        console.log(rows);
     });
 }
