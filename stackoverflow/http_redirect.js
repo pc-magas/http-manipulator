d39e58fd2818c6727e5c76eefe6ac37558a5f801
pcmagas <pcmagas@disroot.org>
2022-12-20 21:22:37 +0200

    302 redirect http.

--- stackoverflow/http_redirect.js
+++ stackoverflow/http_redirect.js
@@ -1,6 +1,9 @@
 const http = require('node:http');
 const server = http.createServer((req,res)=>{
-    
+    const url = "https://reqres.in/api/users?page=2"
+    res.setHeader('location', url);
+    res.writeHead(301)
+    res.end()
 });
 
-server.listen(80);
+server.listen(8080);
