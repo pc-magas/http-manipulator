652ac57af83f764dc254f4f56287257f858f12b5
pcmagas <pcmagas@disroot.org>
2023-01-27 19:36:56 +0200

    Use local path for configuration.

--- src/config.js
+++ src/config.js
@@ -1,19 +1,27 @@
 const fs = require('fs');
-
+const path = require('path');
 /**
  * Load config. Keep in mind it is a syncronous function.
- * @param {String} path Path where config file exists
+ * @param {String} config_path Path where config file exists
  * @returns {Object} with parsed Json 
  */
-function loadConfig(path) {
-    if(!fs.existsSync(path) && !fs.existsSync('/etc/http_manipulator/config.json')){
-        console.error(`Configuration file does not exist ar path ${path}`);
+function loadConfig(config_path) {
+    
+    const path_cwd = path.normalize(path.join(process.cwd(),config_path));
+    
+    //@todo make it simpler
+    if(
+        !fs.existsSync(config_path) && !fs.existsSync('/etc/http_manipulator/config.json') && !fs.existsSync(path_cwd)
+    ){
+        console.error(`Configuration file does not exist ar path ${config_path}`);
         process.exit(-1);
-    } else if(!fs.existsSync(path) && fs.existsSync('/etc/http_manipulator/config.json')){
-        path='/etc/http_manipulator/config.json';
+    } else if(!fs.existsSync(config_path) && !fs.existsSync(path_cwd) && fs.existsSync('/etc/http_manipulator/config.json')){
+        config_path='/etc/http_manipulator/config.json';
+    } else if(!fs.existsSync(config_path) && fs.existsSync(path_cwd)) {
+        config_path = path_cwd;
     }
 
-    const json=JSON.parse(fs.readFileSync(path));
+    const json=JSON.parse(fs.readFileSync(config_path));
 
     // Loading defaults
     json.local_db=json.local_db||'/etc/http_manipulator/db.sqlite'
--- utils/seeder/index.js
+++ utils/seeder/index.js
@@ -1,31 +1,63 @@
+
+const { exit, argv } = require('process');
+
 const db = require('../../src/db.js');
 const configLoader = require('../../src/config.js')
 const seeds = require('./seeds.js');
 
-const config = configLoader();
-const db_path = config.local_db || '/etc/http_manipulator/db.sqlite';
-const database = db(db_path);
+const args_num = process.argv.slice(2).length;
 
-let args = process.argv.slice(2);
-let seeds_to_run=args;
-const args_num = args.length;
+function pargeArgs(){
 
-const help=`
-Run either:
- * For specific seeders (arguments are case sensitive):
-    ${process.argv[0]} seeder1 seeder2 seeder3
- * For all seeders (argument is not case sensitive):
-    ${process.argv[0]} all
-`;
+    const help=`
+    Run either:
+    * For specific seeders (arguments are case sensitive):
+        ${argv[0]} seeder1 seeder2 seeder3
+    * For all seeders (argument is not case sensitive):
+        ${argv[0]} all
+    `;
 
-if(args_num == 0){
-    console.error("You must provide seeders to be run");
-    console.log(help)
-    process.exit(-1);
-}else if(args_num > 0 && args.pop().trim().toLowerCase()=='all'){
-    seeds_to_run=Object.keys(seeds);
+    if(args_num == 0){
+        console.error("You must provide seeders to be run");
+        console.log(help)
+        exit(-1);
+    }
+
+    const arg = require('arg');
+
+    const args = arg({
+        // Types
+        '--help': Boolean,
+        '--config_file': [String],
+        '-c':'--config_file',
+        '--config':'--config_file',
+        '-h':'--help',
+    });
+
+    if(args['--help']){
+        console.log(help);
+        exit(0);
+    }
+
+    return {
+        config_path: args['--config_file'].pop() || null,
+        seeders: args._
+    }
 }
 
+
+const args = pargeArgs();
+
+const config = configLoader(args.config_path);
+const db_path = config.local_db || '/etc/http_manipulator/db.sqlite';
+console.log(`Loading database ${db_path}`)
+const database = db(db_path);
+
+let seeds_to_run=args.seeders;
+
+if(seeds_to_run.pop().trim().toLowerCase()=='all'){
+    seeds_to_run=Object.keys(seeds);
+}
 
 seeds_to_run.forEach((item)=>{
     process.stdout.write(`RUNNING \x1b[36m${item}\x1b[0m......`)
