0eef7492b895c3d4c9466a31cb67e1573d14dc99
pcmagas <pcmagas@disroot.org>
2023-01-30 23:14:37 +0200

    Load Pages.

--- static/js/settings.js
+++ static/js/settings.js
@@ -1,6 +1,15 @@
 $(function(){
-    $(".tab_link").on('click',function(e){
+    $(".page_link").on('click',function(e){
         e.preventDefault();
-        $(this).tab('show');
+        const source = e.target || e.srcElement;
+        const pageId = $(source).attr('href');
+        console.log("Here");
+        showPage(pageId);
     });
 });
+
+function showPage(id){
+    const children = $(id).parent().children(".page");
+    children.each((i,elem)=>$(elem).attr('data-active',false));
+    $(id).attr('data-active',true);
+}
