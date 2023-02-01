f52506721f37a3b1907db6ee492992be9811581a
pcmagas <pcmagas@disroot.org>
2023-02-01 22:54:34 +0200

    Fix Advanced Redirect.

--- static/js/settings.js
+++ static/js/settings.js
@@ -3,8 +3,11 @@ $(function(){
     $("#http_method").on('change',function(event){
         const source = event.target || event.srcElement;
         const http_method = $(source).val();
-
-        if(http_method != 'GET'){
+        console.log(http_method.length,Array.isArray(http_method) ,http_method[0] )
+        if( (Array.isArray(http_method) && (http_method.length > 1 || (http_method.length == 1 && http_method[0] != 'GET')))
+            || (typeof string == 'string' &&  http_method != 'GET')
+        ){
+            console.log("Here");
             $("#status").val('308');
         }
     });
