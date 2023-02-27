99d8cbec172a74cf29df03c4e9677770a4a8a5b9
pcmagas <pcmagas@disroot.org>
2023-02-27 19:17:49 +0200

    Frotnend Save.

--- static/js/settings.js
+++ static/js/settings.js
@@ -1,6 +1,8 @@
 (function( $ ) {
     $.fn.httpRedirect = function() {
 
+        console.log(this);
+
         if(this.get(0).tagName.toLowerCase() != 'select'){
             return this;
         }
@@ -32,7 +34,6 @@
             }
         ];
 
-        console.log(this);
         http_status.forEach((val) => {
             const element = document.createElement("option");
             element.setAttribute("value",val.status);
@@ -44,6 +45,7 @@
     };
 
     $.fn.setHttpStatusBasedOnHttpMethod = function(http_method){
+        
         if(this.get(0).tagName.toLowerCase() != 'select'){
             return this;
         }
@@ -51,11 +53,35 @@
         if( (Array.isArray(http_method) && (http_method.length > 1 || (http_method.length == 1 && http_method[0] != 'GET')))
             || (typeof string == 'string' &&  http_method != 'GET')
         ){
-            console.log("Here");
             $(this).val('308');
         }
         return this;
     }
+
+    $.fn.onSubmitAjax = function(success,fail,always){
+
+        if(this.get(0).tagName.toLowerCase() != 'form'){
+            return this;
+        }
+
+        $(this).on('submit',function(e){
+            e.preventDefault();
+            console.log($(this).serialize());
+            console.log($(this).attr('action'));
+            console.log($(this).attr('method'));
+            $.ajax({
+                "action": $(this).attr('action'),
+                "method": $(this).attr('method'),
+                "data": $(this).serialize(),
+                "success": success,
+                "fail":fail,
+                "always":always
+            });
+        });
+
+    }
  
+    
 }( jQuery ));
- 
+
+
