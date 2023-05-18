9089ebd137c60cb452f84d5b1302161b878c2834
pcmagas <pcmagas@disroot.org>
2023-05-18 21:38:59 +0300

    Test Img to Upload and Dirty Code.

--- stackoverflow/connect_expirement.js
+++ stackoverflow/connect_expirement.js
@@ -1,43 +1,35 @@
 const connect = require('connect');
-// const {detectBodyMime} = require('../src/common/http_utils');
-const fs = require('fs');
+const {detectBodyMime} = require('../src/common/http_utils');
+const {Buffer} = require("node:buffer");
 
 const app = connect();
-
-const detectMultipart = (string)=>{
-    return string.indexOf('Content-Disposition: form-data; name="')>1;
-};
 
 app.use(function(req,res,next){
     req.id = 1;
 
     var body = [];
 
-    req.on('data',(data) =>
-    {
-        console.log('Getting Body');
-        body.push(data)
-    });
+    req.on('data',(data) => body.push(data) );
 
     req.on('end',() => {
 
-        console.log(req.headers);
         body = Buffer.concat(body).toString();
-        fs.writeFileSync('./'+Date.now(),body);
         
-        console.log("\n###########\n\n");
-        console.log(detectMultipart(body));
-        console.log("\n\n###########\n\n");
+        detectBodyMime(body,(err,mime,extention,buffer)=>{
+            res.setHeader("Content-Type",'text/plain');
+            res.writeHead(200,{'X-val':3});
 
-        next();
+            res.end("MIME: "+mime+"\n"+"Suggested file extention: "+extention+"\n");
+        });
+
     });
 });
 
-app.use(function(req,res){
-    console.log("id:",req.id);
-    res.setHeader("Content-Type",'text/plain');
-    res.writeHead(200,{'X-val':3});
-    res.end("jhello");
-});
+// app.use(function(req,res){
+//     console.log("id:",req.id);
+//     res.setHeader("Content-Type",'text/plain');
+//     res.writeHead(200,{'X-val':3});
+//     res.end("jhello");
+// });
 
 app.listen(8090)
