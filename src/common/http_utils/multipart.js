8662fb5806b680c5dee5a50ac59f3c3f2961cee2
pcmagas <pcmagas@disroot.org>
2023-06-06 21:13:35 +0300

    Basic Multipart Parse.

--- src/common/http_utils/multipart.js
+++ src/common/http_utils/multipart.js
@@ -5,7 +5,7 @@
  * @param {*} boundary 
  * @param {*} callback 
  */
-const parseMultipart = (body,boundary) => {
+const parseMultipart = (body,boundary,fieldCallback) => {
 
     const bodyToParse = body.trim();
 
@@ -24,13 +24,61 @@ const parseMultipart = (body,boundary) => {
     const fields = body.split("--"+boundary);
 
     let fieldCount=0;
+
     fields.forEach((item)=>{
         if(item.trim()=='--' || item.trim() == ''){
             return;
         }
         fieldCount++;
+        
         console.log("FIELD");
-        console.log(item);
+        
+        item = item.trim();
+
+        const newlinePos = item.search('\r\n');
+        let fieldInfo = item.substring(0,newlinePos).trim(); 
+        
+        item = item.substring(newlinePos).trim();
+        
+        let contentTypeLine = null;
+
+        const contentTypePosStart = item.search("Content-Type");
+
+        if(contentTypePosStart >= 0){
+            const contentTypeNewLinePos = item.indexOf('\r\n',contentTypePosStart);
+
+            contentTypeLine=item.substring(contentTypePosStart,contentTypeNewLinePos).trim();
+            item=item.substring(contentTypeNewLinePos);
+
+            contentTypeLine=contentTypeLine.replace('Content-Type: ','').trim();
+        }
+
+        item = item.trim();
+
+              
+
+        fieldInfo = fieldInfo.replace("Content-Disposition: form-data; ",'').trim();
+        
+        const isFile = fieldInfo.indexOf('; filename=') > 0;
+
+        fieldInfo = fieldInfo.split(';');
+
+        const fieldName = fieldInfo[0].replace("name=\"",'').replace(/\"$/,"");
+        let filename = (isFile)?fieldInfo[1].replace('filename="',"").replace(/\"$/,"").trim():null; 
+
+         /**
+         * item: data
+         * contentTypeLine: contentType mime
+         * Field Values: fieldInfo
+         * Filename:  filename
+         * FieldName: fieldName
+         */ 
+
+        console.log("LINE",fieldInfo);
+        console.log("CONTENT tYPE",contentTypeLine);
+        console.log("NAME",fieldName);
+        console.log("FILENAME",filename);
+        console.log("DATA",item);
     });
 
 }
